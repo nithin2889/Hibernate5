5d6be8fe67ee753900f0ac8f442fc315b93756e4
Nithin Prasad <nithin2889@gmail.com>
2017-12-25 20:21:16 -0600

    Hibernate Criteria Query With Multiple Roots Example.

--- 036-HibernateCriteriaQueryWithMultipleRootExample/src/main/java/com/javabotanist/client/SaveDataClientTest.java
+++ 036-HibernateCriteriaQueryWithMultipleRootExample/src/main/java/com/javabotanist/client/SaveDataClientTest.java
@@ -3,8 +3,8 @@ package com.javabotanist.client;
 import java.util.Date;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
+import com.javabotanist.entities.Partner;
 import com.javabotanist.entities.Person;
 import com.javabotanist.entities.Phone;
 import com.javabotanist.entities.PhoneType;
@@ -13,59 +13,56 @@ import com.javabotanist.util.HibernateUtil;
 public class SaveDataClientTest {
 
 	public static void main(String[] args) {
-
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = null;
-
-		try {
-			session = sf.openSession();
+		
+		try(Session session = HibernateUtil.getSessionFactory().openSession()) {
 			session.beginTransaction();
-
+			
 			Person person1 = new Person();
 			person1.setName("Mark Bingel");
 			person1.setNickName("Mac");
 			person1.setAddress("Alameda Street Los Angeles");
 			person1.setCreatedOn(new Date());
 			person1.setVersion(1);
-
+			
 			Phone phone1 = new Phone();
 			phone1.setNumber("9073637380");
 			phone1.setType(PhoneType.MOBILE);
 			phone1.setPerson(person1);
-
+			
 			person1.getPhones().add(phone1);
-
+			
 			Person person2 = new Person();
 			person2.setName("Sean Murphy");
 			person2.setNickName("Sam");
 			person2.setAddress("Bank of Canada,234 Wellington Street");
 			person2.setCreatedOn(new Date());
 			person2.setVersion(1);
-
+			
 			Phone phone2 = new Phone();
 			phone2.setNumber("809865430");
 			phone2.setType(PhoneType.MOBILE);
 			phone2.setPerson(person2);
-
+			
 			Phone phone3 = new Phone();
 			phone3.setNumber("022909742");
 			phone3.setType(PhoneType.LAND_LINE);
 			phone3.setPerson(person2);
-
+			
 			person2.getPhones().add(phone2);
 			person2.getPhones().add(phone3);
-
+			
+			Partner partner = new Partner();
+			partner.setName("Nitin P");
+			partner.setVersion(1);
+			
 			session.save(person1);
 			session.save(person2);
-
+			session.save(partner);
+			
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			if (session != null) {
-				session.close();
-			}
-		}
+		} 
 	}
 
 }
