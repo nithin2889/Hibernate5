0d404d08bf49a7203ab18b43a4b7f50d5565f00f
Nithin Prasad <nithin2889@gmail.com>
2017-12-25 20:10:29 -0600

    Hibernate Criteria Query With Multiple Roots Example.

---
+++ 036-HibernateCriteriaQueryWithMultipleRootExample/src/main/java/com/javabotanist/client/HibernateCriteriaQueryWithMultipleRootClientTest.java
@@ -0,0 +1,31 @@
+package com.javabotanist.client;
+
+import java.sql.CallableStatement;
+import java.sql.Types;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.hibernate.Session;
+
+import com.javabotanist.util.HibernateUtil;
+
+public class HibernateCriteriaQueryWithMultipleRootClientTest {
+
+	public static void main(String[] args) {
+		try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+			int personId = 1;
+			final AtomicReference<Integer> phoneCount = new AtomicReference<>();
+			session.doWork(connection -> {
+				try(CallableStatement callableStatement = connection.prepareCall("{ ? = call fn_count_phones(?) }")) {
+					callableStatement.registerOutParameter(1, Types.INTEGER);
+					callableStatement.registerOutParameter(2, personId);
+					callableStatement.execute();
+					phoneCount.set(callableStatement.getInt(1));
+				}
+			});
+			if(phoneCount != null) {
+				System.out.println("Phone Count: " + phoneCount.get());
+			}
+		}
+	}
+
+}
