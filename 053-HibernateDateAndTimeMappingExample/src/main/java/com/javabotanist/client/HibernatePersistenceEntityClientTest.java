a357238e0ea3293f83c788e2c8c3b62f3940692c
Nithin Prasad <nithin2889@gmail.com>
2017-12-27 22:46:22 -0600

    Hibernate Date And Time Mapping Example.

---
+++ 053-HibernateDateAndTimeMappingExample/src/main/java/com/javabotanist/client/HibernatePersistenceEntityClientTest.java
@@ -0,0 +1,44 @@
+package com.javabotanist.client;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import com.javabotanist.entities.Book;
+import com.javabotanist.entities.Person;
+import com.javabotanist.util.HibernateUtil;
+
+public class HibernatePersistenceEntityClientTest {
+
+	public static void main(String[] args) {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			Person author1 = new Person();
+			author1.setName("Gavin King");
+			
+			Date publishedDate = new Date();
+			
+			Book book1 = new Book();
+			book1.setIsbn("978-9730228236");
+			book1.setTitle("Hibernate High-Performance Java Persistence");
+			book1.setPublishedDate(publishedDate);
+			book1.setAuthor(author1);
+			
+			Book book2 = new Book();
+			book2.setIsbn("900-9730228524");
+			book2.setTitle("Hibernate Persistence Contexts");
+			book2.setPublishedDate(publishedDate);
+			book2.setAuthor(author1);
+			
+			author1.getBooks().add(book1);
+			author1.getBooks().add(book2);
+			
+			session.beginTransaction();
+			session.save(author1);
+			
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ 053-HibernateDateAndTimeMappingExample/src/main/java/com/javabotanist/entities/Book.java
@@ -0,0 +1,81 @@
+package com.javabotanist.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name="book")
+public class Book {
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+	@Column(name="title",length=100)
+    private String title;
+
+    @NaturalId
+    private String isbn;
+
+    @ManyToOne
+    private Person author;
+
+    // @Temporal(TemporalType.TIME)
+    // @Temporal(TemporalType.TIMESTAMP)
+    @Temporal(TemporalType.DATE)
+    @Column(name="published_date")
+    private Date publishedDate;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Person getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Person author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+	public Date getPublishedDate() {
+		return publishedDate;
+	}
+
+	public void setPublishedDate(Date publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+    
+}
