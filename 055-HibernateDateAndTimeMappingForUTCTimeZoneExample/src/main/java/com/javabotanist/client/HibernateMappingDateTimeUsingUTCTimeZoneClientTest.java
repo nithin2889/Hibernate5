f3136a66b980aa064c2c1c1fd58f621156ebbb35
Nithin Prasad <nithin2889@gmail.com>
2017-12-28 00:00:51 -0600

    Hibernate Date And Time Mapping For UTC Time Zone Example.

---
+++ 055-HibernateDateAndTimeMappingForUTCTimeZoneExample/src/main/java/com/javabotanist/client/HibernateMappingDateTimeUsingUTCTimeZoneClientTest.java
@@ -0,0 +1,29 @@
+package com.javabotanist.client;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import com.javabotanist.entities.Book;
+import com.javabotanist.entities.Person;
+import com.javabotanist.util.HibernateUtil;
+
+public class HibernateMappingDateTimeUsingUTCTimeZoneClientTest {
+
+	public static void main(String[] args) {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			Long personId = 1L;
+			Person person = session.get(Person.class, personId);
+			System.out.println(":::::Person Details:::::");
+			System.out.println(person.getId() + "\t" + person.getName());
+			System.out.println(":::::List Of Books:::::");
+			List<Book> books = person.getBooks();
+			for (Book book : books) {
+				System.out.println(book.getId()+"\t"+book.getTitle()+"\t"+book.getIsbn());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
