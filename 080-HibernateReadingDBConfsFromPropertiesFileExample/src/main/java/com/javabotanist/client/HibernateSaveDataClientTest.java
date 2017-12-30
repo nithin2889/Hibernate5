9d3095fdbc465471a6dd8fd362e17afe48d91590
Nithin Prasad <nithin2889@gmail.com>
2017-12-29 19:27:33 -0600

    Hibernate Reading DB Config From Properties File Example.

---
+++ 080-HibernateReadingDBConfsFromPropertiesFileExample/src/main/java/com/javabotanist/client/HibernateSaveDataClientTest.java
@@ -0,0 +1,39 @@
+package com.javabotanist.client;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.javabotanist.entities.Employee;
+import com.javabotanist.util.HibernateUtil;
+
+public class HibernateSaveDataClientTest {
+
+	public static void main(String[] args) {
+		Transaction tx = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			tx = session.beginTransaction();
+
+			Employee employee1 = new Employee();
+			employee1.setEmployeeName("Sean Murphy");
+			employee1.setUserName("seanm");
+			employee1.setPassword("pass#123");
+			employee1.setAccessLevel(1);
+
+			Employee employee2 = new Employee();
+			employee2.setEmployeeName("Barry Johnson");
+			employee2.setUserName("barryj");
+			employee2.setPassword("barry@123");
+			employee2.setAccessLevel(1);
+			
+			session.persist(employee1);
+			session.persist(employee2);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive())
+				tx.rollback();
+			throw e;
+		}
+	}
+
+}
---
+++ 080-HibernateReadingDBConfsFromPropertiesFileExample/src/main/java/com/javabotanist/entities/Employee.java
@@ -0,0 +1,73 @@
+package com.javabotanist.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="employee_name",length=60)
+    private String employeeName;
+    
+    @NaturalId
+    @Column(name="user_name",length=60,nullable=false)
+    private String username;
+    
+    @Column(name="password",nullable=false)
+    private String password;
+    
+    @Column(name="access_level")
+    private int accessLevel;
+    
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUserName(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setAccessLevel(int accessLevel) {
+		this.accessLevel = accessLevel;
+	}
+	
+	public int getAccessLevel() {
+		return accessLevel;
+	}
+}
